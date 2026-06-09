b4d72d23ade7afb4ee3dbe925893428157841c3a
lvpragaspathy <pragaspathy@wisc.edu>
2026-06-09 15:20:46 -0500

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,27 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        def helper(x):
+            return ord(x) - ord('a')
+
+        for string in strs:
+            profile = [0] * 26
+
+            for char in string:
+                profile[helper(char)] += 1
+
+            groups[tuple(profile)].append(string)
+
+        return list(groups.values())
+
+
+
+
+
+            
+
+
+
+
+        
