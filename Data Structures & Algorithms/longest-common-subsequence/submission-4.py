9e723e4c16a3b36a9f2590dcbab5125a931f1471
lvpragaspathy <pragaspathy@wisc.edu>
2026-07-30 11:56:17 -0500

    Add: longest-common-subsequence - submission-4

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-4.py
@@ -0,0 +1,34 @@
+from functools import cache
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
+        # For any character in text1, we need to find if it exists in text2  after the pointer. We need to chose if we want to keep it or not
+
+
+        
+        if not text1 or not text2:
+            return 0
+
+        mem = defaultdict(int)
+        
+        # i: text1 pointer, j: text2 pointer
+       # @cache
+        def dp(i, j) -> int:
+            if i > len(text1)-1 or j > len(text2)-1:
+                return 0
+            
+            if (i, j) in mem:
+                return mem[(i, j)]
+
+            if text1[i] == text2[j]:
+                mem[(i, j)] = 1 + dp(i+1, j+1)
+
+            else:
+                mem[(i, j)] = max(dp(i+1, j), dp(i, j+1))
+
+            return mem[(i, j)]
+
+
+        return dp(0, 0)
+
